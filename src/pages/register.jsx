51ea8e237b569a00a3739b84b6cd5fc4ba779f7a
LE HIEU HIEP <hieuhiep.ntqn88@gmail.com>
2024-10-27 19:22:19 +0700

    video 90 uncontrol component, form.get/setFieldssetFields

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,11 +1,27 @@
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, notification } from "antd";
 import { useForm } from "antd/es/form/Form";
+import { registerUserAPI } from "../services/api.service";
+import { useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
     const [form] = useForm();
-
-    const onFinish = (values) => {
-        console.log('>> ', values)
+    const navigate = useNavigate();
+    const onFinish = async (values) => {
+        const res = await registerUserAPI(
+            values.fullName, values.email, values.password, values.phone
+        );
+        if (res.data) {
+            notification.success({
+                message: "Register User",
+                description: "Register OK nhé "
+            });
+            navigate("/login");
+        } else {
+            notification.error({
+                message: "Register User",
+                description: JSON.stringify(res.message)
+            });
+        }
     }
     return (
         <Form
@@ -22,48 +38,57 @@ const RegisterPage = () => {
                 <Form.Item
                     label="Full name"
                     name="fullName"
-                // rules={[
-                //     {
-                //         required: true,
-                //         message: 'Please input your username!',
-                //     },
-                // ]}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your fullName!',
+                        },
+                    ]}
                 >
                     <Input />
                 </Form.Item>
                 <Form.Item
                     label="Email"
                     name="email"
-                // rules={[
-                //     {
-                //         required: true,
-                //         message: 'Please input your username!',
-                //     },
-                // ]}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your email!',
+                        },
+                        {
+                            type: "email",
+                            message: "The input is not valid E-mail!",
+                        },
+                    ]}
                 >
                     <Input />
                 </Form.Item>
                 <Form.Item
                     label="Password"
                     name="password"
-                // rules={[
-                //     {
-                //         required: true,
-                //         message: 'Please input your username!',
-                //     },
-                // ]}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password!',
+                        },
+                        {
+                            min: 6,
+                            message: 'Min character is 6 !',
+                        },
+                    ]}
                 >
                     <Input.Password />
                 </Form.Item>
                 <Form.Item
                     label="Phone number"
                     name="phone"
-                // rules={[
-                //     {
-                //         required: true,
-                //         message: 'Please input your username!',
-                //     },
-                // ]}
+                    rules={[
+                        {
+                            message: 'Please input only number!',
+                            pattern: new RegExp(/^[0-9]+$/)
+                        },
+
+                    ]}
                 >
                     <Input />
                 </Form.Item>
